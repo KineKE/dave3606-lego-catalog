1f76a5151321a457e55989b78ea09b0418cc28ad
Kine K Engseth <e_kine@hotmail.com>
2026-03-24 16:11:13 +0100

    Initial working project setup

---
+++ report.md
@@ -0,0 +1,3 @@
+# DAVE3606 - Resource-Efficient Programs Project - 2026
+## Kine Kragl Engseth - s330526 - kieng6560
+
---
+++ static/script.js
@@ -0,0 +1,9 @@
+const jsConfetti = new JSConfetti()
+
+function legoRain() {
+    jsConfetti.addConfetti({
+        emojis: ['🧱', '🟨', '🟥', '🟦'],
+        emojiSize: 30,
+        confettiNumber: 40
+    })
+}
