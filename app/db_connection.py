4bf5372526b00a991839d0694ec3426b17b12521
Kine K Engseth <e_kine@hotmail.com>
2026-04-03 18:37:54 +0200

    Refactoring the database connection

--- app/db_connection.py
+++ app/db_connection.py
@@ -4,21 +4,49 @@ Module text goes here.
 
 import os
 import psycopg
-from dotenv import load_dotenv
 
-load_dotenv()
+class Database:
+    def __init__(self):
+        self.host = None
+        self.post = None
+        self.name = None
+        self.user = None
+        self.password = None
+        self.connection = None
+        self.cursor = None
+
+    def __repr__(self):
+        return f"Database ('{self.host, self.post, self.name}')"
+
+    def get_db_config(self):
+        pass
 
 
-def get_connection():
-    """
-    A database connection wrapper.
-    :return:
-    """
-    #TODO: Check for missing values
-    return psycopg.connect(
-        host=os.getenv("DB_HOST"),
-        port=int(os.getenv("DB_PORT")),
-        dbname=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD")
-    )
+
+
+
+
+
+    #
+    # def get_config(self):
+    #     pass
+    #
+    #
+    # def get_cursor(self):
+    #     pass
+    #
+    # def get_connection(self):
+    #     pass
+    #
+    #
+
+    #
+    # def get_db_config(self):
+    #     #TODO: Check for missing values
+    #     return psycopg.connect(
+    #         host=os.getenv("DB_HOST"),
+    #         port=int(os.getenv("DB_PORT")),
+    #         dbname=os.getenv("POSTGRES_DB"),
+    #         user=os.getenv("POSTGRES_USER"),
+    #         password=os.getenv("POSTGRES_PASSWORD")
+    #     )
